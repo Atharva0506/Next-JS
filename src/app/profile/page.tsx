9f7a5bc2a017b9912c63759fc762593c79fe71a1
Atharva Naik <atharvan.coder@gmail.com>
2023-08-16 12:20:25 +0530

    Logout button route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+import axios  from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 export default function ProfilePage(){
+    const router = new useRouter();
+    const logout = async ()=>{
+        try {
+            await axios.get("api/users/logout")
+            console.log('Logout successful')
+            router.push("/login")
+        } catch (error:any) {
+            console.log(error);
+        }
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
             <h1>Profile </h1>
             <hr />
             <p>Profile Page</p>
+
+            <button onClick={logout}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Logout</button>
         </div>
     )
 }
