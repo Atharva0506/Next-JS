b32cdf5f0e09c142ac2be6fb05634034916783e6
Atharva Naik <atharvan.coder@gmail.com>
2023-08-17 16:30:14 +0530

    Data from Token

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,8 +2,10 @@
 import axios  from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 export default function ProfilePage(){
     const router = new useRouter();
+    const [data,setData] = useState("Null")
     const logout = async ()=>{
         try {
             await axios.get("api/users/logout")
@@ -13,12 +15,18 @@ export default function ProfilePage(){
             console.log(error);
         }
     }
+
+    const getUserDetails = async ()=>{
+        const res = await axios.get("api/users/me")
+        console.log(res.data);
+        setData(res.data.data._id);
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
             <h1>Profile </h1>
             <hr />
             <p>Profile Page</p>
-
+            <h2>{data?"":""}</h2>
             <button onClick={logout}
             className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Logout</button>
         </div>
